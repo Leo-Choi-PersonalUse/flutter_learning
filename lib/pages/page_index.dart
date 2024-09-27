376abdb3338f3063ce6f6508b8144343e118f85f
Leo Choi <leochoi@fseng.com.hk>
2024-09-27 15:34:32 +0800

    add prop test page

--- lib/pages/page_index.dart
+++ lib/pages/page_index.dart
@@ -1,2 +1,3 @@
 export "landing_page.dart";
 export "menu_page.dart";
+export "prop_test_page.dart";
---
+++ lib/pages/prop_test_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class PropTestPage extends StatefulWidget {
+  const PropTestPage({super.key});
+
+  @override
+  State<PropTestPage> createState() => _PropTestPageState();
+}
+
+class _PropTestPageState extends State<PropTestPage> {
+  @override
+  String page_str = "hello World";
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prop Test Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TestWidget(test_str: page_str, add: add),
+            IconButton(
+                onPressed: () {
+                  add();
+                },
+                icon: Icon(Icons.add)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void add() {
+    setState(() {
+      page_str += "d";
+    });
+    print(page_str);
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  String test_str;
+  Function? add;
+
+  TestWidget({Key? key, required this.test_str, this.add}) : super(key: key);
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  late String test_str_widget_state;
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    test_str_widget_state = widget.test_str;
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text("Parent: " + widget.test_str),
+        Text("Child: " + test_str_widget_state),
+        IconButton(
+            onPressed: () {
+              if (widget.add != null) widget.add!();
+            },
+            icon: Icon(Icons.safety_check))
+      ],
+    );
+  }
+}
